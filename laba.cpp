900122959c61410668b365d3de00c82008982ea2
SadBlueYet <88283979+SadBlueYet@users.noreply.github.com>
2022-11-08 09:41:02 +0700

    Add files via upload

---
+++ laba.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	int R = 10;
+	const int len = 10;
+	int numberArray[len];
+	std::cout << "enter 10 numbers: ";
+	for (int i = 0; i < len; i++) std::cin >> numberArray[i];
+	
+	for (int i = 0; i < len; i++)
+	{
+		std::cout << numberArray[i] << ' ';
+		for (int j = 0; j < len; j++) std::cout << numberArray[i] - numberArray[j] << ' ' << '\t';
+
+		std::cout << '\n';
+	}
+	return 0;
+}
